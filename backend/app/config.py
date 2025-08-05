8365062e26facb2f41cd78f298da05e6827f8f90
Kishan <ksp1510@gmail.com>
2025-08-04 22:16:43 -0400

    Updated mongoDB URI

--- backend/app/config.py
+++ backend/app/config.py
@@ -6,7 +6,7 @@ import boto3 # type: ignore
 # Load environment variables
 load_dotenv()
 
-MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://localhost:27017")
+MONGODB_URL = os.getenv("MONGODB_URL", "mongodb+srv://ksp1510:Ksp1510@cluster0.id9hp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.getenv("DB_NAME", "Applicants")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGODB_URL)
